ab2c821502dbc1e142059789541b09875a18bd51
Clark Nguyen <mincoding@gmail.com>
2018-01-14 16:47:08 +0700

    Response an image from storage folder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +18,27 @@ Route::get('/', function () {
 });
 
 Route::get('task/list', 'TaskController@index')->name('task.list');
+
+//go to browser to test via http://localhost:8000/storage/gtx1060.jpg
+Route::get('storage/{filename}', function($filename){
+    $path = storage_path($filename);
+    if ( !File::exists($path) ) {
+        abort(404);
+    }
+    $file = File::get($path);
+    $type = File::mimeType($path);
+    $headers = [
+        'Content-Type' => $type
+    ];
+    //1st way to response an image
+    $response = Response::make($file, 200);
+    $response->header('Content-Type', $type);
+    return $response;
+
+    //2nd way to response an image
+    return Response::stream(function() use($file) {
+        echo $file;
+    }, 200, $headers);
+
+
+});
